c44b7f401317fd793293c68a7435cb85f90ebbe0
En Shih <seanstone5923@gmail.com>
2016-07-23 21:36:47 +0800

    PulsarMath Vec

--- Pulsar/include/PulsarMath.hpp
+++ Pulsar/include/PulsarMath.hpp
@@ -25,22 +25,22 @@
 
 #include <Quaternion.hpp>
 
+typedef glm::vec2 vec2;
+typedef glm::vec3 vec3;
+typedef glm::vec4 vec4;
+typedef glm::mat2 mat2;
+typedef glm::mat3 mat3;
+typedef glm::mat4 mat4;
+
+typedef vec4 float4;
+typedef vec3 float3;
+typedef vec2 float2;
+typedef glm::ivec4 int4;
+typedef glm::ivec3 int3;
+typedef glm::ivec2 int2;
+
 namespace Pulsar
 {
-	typedef glm::vec2 vec2;
-	typedef glm::vec3 vec3;
-	typedef glm::vec4 vec4;
-	typedef glm::mat2 mat2;
-	typedef glm::mat3 mat3;
-	typedef glm::mat4 mat4;
-
-	typedef vec4 float4;
-	typedef vec3 float3;
-	typedef vec2 float2;
-	typedef glm::ivec4 int4;
-	typedef glm::ivec3 int3;
-	typedef glm::ivec2 int2;
-
 	using namespace glm;
 
     inline float4 cross(float4 u, float4 v)
