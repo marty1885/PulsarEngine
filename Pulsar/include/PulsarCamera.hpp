68bdc00f058282e5b959a21aefd9b88ad8c2e00e
En Shih <seanstone5923@gmail.com>
2016-07-24 12:36:53 +0800

    PulsarRay

--- Pulsar/include/PulsarCamera.hpp
+++ Pulsar/include/PulsarCamera.hpp
@@ -2,6 +2,7 @@
 #define PULSARCAMERA_HPP
 
 #include <PulsarMath.hpp>
+#include <PulsarRay.hpp>
 
 namespace Pulsar
 {
@@ -16,35 +17,6 @@ namespace Pulsar
     	float width;
     	float height;
     	float fov;
-    };
-
-    class Ray
-    {
-    public:
-    	Ray(){}
-    	Ray(vec3 ori, vec3 dir)
-    	{
-    		origin = ori;
-    		direction = dir;
-    	}
-
-    	vec3 origin;
-    	vec3 direction;
-
-    	void calcSign(int* sign) const
-    	{
-    		sign[0] = direction[0] < 0;
-    		sign[1] = direction[1] < 0;
-    		sign[2] = direction[2] < 0;
-    	}
-
-    	/*inline float4 inverseDirection() const
-    	{
-    		//Workarround a bug/unlikelly issue in Ray-AABB intersection caused by inf in inverseDirection.
-    		float4 inverseDirection = 1.0f/direction;
-    		inverseDirection = inverseDirection + float4(glm::equal(direction,float4(0,0,0,1)))*float4(FLT_EPSILON);
-    		return inverseDirection;
-    	}*/
     };
 
     class Camera
